6253c83950977ae6d03a64c2fdba8348b9052305
Glen Takahashi <glen@glentaka.com>
2020-11-16 15:34:22 -0800

    bump alb ingress controller

--- aws/modules/alb/alb.tf
+++ aws/modules/alb/alb.tf
@@ -1,6 +1,6 @@
 module "alb_ingress_controller" {
   source  = "iplabs/alb-ingress-controller/kubernetes"
-  version = "3.1.0"
+  version = "3.4.0"
 
   k8s_cluster_type = "eks"
   k8s_namespace    = "kube-system"
