5f16daea983ed79aad64f69656506d13444f3f12
Glen Takahashi <glen@glentaka.com>
2021-01-11 19:40:14 -0800

    Fix providers for 0.13

--- aws/versions.tf
+++ aws/versions.tf
@@ -1,3 +1,14 @@
 terraform {
-  required_version = ">= 0.13.0, < 0.14.0"
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    helm = {
+      source = "hashicorp/helm"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
 }
