e14eb556e2f4558c80f42539c2b4b214f3f5226d
Glen Takahashi <glen@glentaka.com>
2021-02-03 12:10:27 -0800

    add k8s auth for managing cluster

--- aws/eks.tf
+++ aws/eks.tf
@@ -63,3 +63,20 @@ module "eks" {
     },
   ]
 }
+
+// Needed for managing auth
+data "aws_eks_cluster" "hex" {
+  name = module.hex.cluster_id
+}
+
+data "aws_eks_cluster_auth" "hex" {
+  name = module.hex.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.hex.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.hex.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.hex.token
+  load_config_file       = false
+  version                = "~> 1.9"
+}
