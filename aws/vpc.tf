1aa07771035f3b6d278f957292975bcb9e957480
Glen Takahashi <glen@glentaka.com>
2021-03-16 16:08:21 -0700

    Remove azs

--- aws/vpc.tf
+++ aws/vpc.tf
@@ -1,13 +1,9 @@
-data "aws_availability_zones" "available" {
-}
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 2.70"
 
   name            = var.name
   cidr            = "10.0.0.0/16"
-  azs             = data.aws_availability_zones.available.names
   private_subnets = ["10.0.10.0/24", "10.0.20.0/24", "10.0.30.0/24"]
   public_subnets  = ["10.0.110.0/24", "10.0.120.0/24", "10.0.130.0/24"]
 
