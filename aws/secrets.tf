e67165fd262023f72be0bfa265b0bb374b9e1dc6
Glen Takahashi <glen@glentaka.com>
2021-03-16 13:03:07 -0700

    Unencrypted

--- aws/secrets.tf
+++ aws/secrets.tf
@@ -6,7 +6,7 @@ resource "aws_secretsmanager_secret_version" "hex-secrets" {
   secret_id = aws_secretsmanager_secret.hex-secrets.id
   secret_string = jsonencode({
     eks = {
-      "AWS_SECRET_ACCESS_KEY" = aws_iam_access_key.eks-user.encrypted_secret
+      "AWS_SECRET_ACCESS_KEY" = aws_iam_access_key.eks-user.secret
       "AWS_ACCESS_KEY_ID"     = aws_iam_access_key.eks-user.id
       cluster                 = module.eks.cluster_id
     }
