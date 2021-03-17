40a309bf4c9cf872490683ebd14c626e28375264
Glen Takahashi <glen@glentaka.com>
2021-03-17 10:24:28 -0700

    use count

--- aws/route53.tf
+++ aws/route53.tf
@@ -5,13 +5,13 @@ resource "aws_route53_zone" "hex" {
 data "aws_elb_hosted_zone_id" "hex" {}
 
 resource "aws_route53_record" "hex" {
-  for_each = var.alb_url != null ? toset([var.alb_url]) : []
-  name     = "${var.domain_name}."
-  type     = "A"
-  zone_id  = aws_route53_zone.hex.zone_id
+  count   = var.alb_url != null ? 1 : 0
+  name    = "${var.domain_name}."
+  type    = "A"
+  zone_id = aws_route53_zone.hex.zone_id
 
   alias {
-    name                   = each.value
+    name                   = var.alb_url
     zone_id                = data.aws_elb_hosted_zone_id.hex.id
     evaluate_target_health = true
   }
