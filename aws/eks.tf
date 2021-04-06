9e1d3de645057d49579a4fd43903c2e21af6ddac
Glen Takahashi <glen@glentaka.com>
2021-04-06 13:03:19 -0700

    Add helm provider

--- aws/eks.tf
+++ aws/eks.tf
@@ -99,19 +99,3 @@ module "eks" {
     },
   ]
 }
-
-// Needed for managing auth
-data "aws_eks_cluster" "hex" {
-  name = module.eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "hex" {
-  name = module.eks.cluster_id
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.hex.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.hex.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.hex.token
-  load_config_file       = false
-}
--- aws/providers.tf
+++ aws/providers.tf
@@ -1,3 +1,28 @@
 provider "aws" {
   region = var.region
 }
+
+data "aws_eks_cluster" "hex" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "hex" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.hex.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.hex.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.hex.token
+  load_config_file       = false
+}
+
+provider "helm" {
+  // TODO: dedupe? with above?
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    load_config_file       = false
+  }
+}
