d7237cd5567955f9bae3dbd7616a1c8c79d0bd49
Glen Takahashi <glen@glentaka.com>
2021-04-06 13:03:54 -0700

    Fix cluster names

--- aws/providers.tf
+++ aws/providers.tf
@@ -20,9 +20,9 @@ provider "kubernetes" {
 provider "helm" {
   // TODO: dedupe? with above?
   kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
+    host                   = data.aws_eks_cluster.hex.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.hex.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.hex.token
     load_config_file       = false
   }
 }
