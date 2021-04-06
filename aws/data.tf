84ae68901abf6ec862f0e992cd8b49ff0d2c4996
Glen Takahashi <glen@glentaka.com>
2021-04-06 13:11:37 -0700

    Standardize helm configs

--- aws/data.tf
+++ aws/data.tf
@@ -1,4 +1,13 @@
 data "aws_caller_identity" "current" {}
 
-data "aws_availability_zones" "available" {
+data "aws_availability_zones" "available" {}
+
+data "aws_region" "current" {}
+
+data "aws_eks_cluster" "hex" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "hex" {
+  name = module.eks.cluster_id
 }
