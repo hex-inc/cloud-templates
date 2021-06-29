8d075ef8b9da318a5e67870277adcb593ee06397
Claire Abu-Assal <claire@hex.tech>
2021-06-29 16:05:07 -0700

    Update dashboard to use new widgets.

--- aws/monitoring_dashboards.tf
+++ aws/monitoring_dashboards.tf
@@ -1,52 +1,47 @@
 resource "newrelic_one_dashboard" "overview" {
   count = var.monitoring_enabled ? 1 : 0
   title = "Overview of ${local.monitoring_cluster_name}"
+  page {
+    widget_line {
+      title         = "Cluster CPU"
+      nrql          = "SELECT average(allocatableCpuCoresUtilization) AS 'CPU Utilization' FROM K8sNodeSample WHERE clusterName = '${var.name}' TIMESERIES AUTO"
+      row           = 1
+      column        = 1
+    }
 
-  widget {
-    title         = "Cluster CPU"
-    visualization = "faceted_line_chart"
-    nrql          = "SELECT average(allocatableCpuCoresUtilization) AS 'CPU Utilization' FROM K8sNodeSample WHERE clusterName = '${var.name}' TIMESERIES AUTO"
-    row           = 1
-    column        = 1
-  }
+    widget_line {
+      title         = "Cluster Disk"
+      nrql          = "SELECT average(fsCapacityUtilization) AS 'FS Utilization' FROM K8sNodeSample WHERE clusterName = '${var.name}' TIMESERIES AUTO"
+      row           = 1
+      column        = 2
+    }
 
-  widget {
-    title         = "Cluster Disk"
-    visualization = "faceted_line_chart"
-    nrql          = "SELECT average(fsCapacityUtilization) AS 'FS Utilization' FROM K8sNodeSample WHERE clusterName = '${var.name}' TIMESERIES AUTO"
-    row           = 1
-    column        = 2
-  }
+    widget_line {
+      title         = "Cluster Memory"
+      nrql          = "SELECT average(allocatableMemoryUtilization) AS 'Memory Utilization' FROM K8sNodeSample WHERE clusterName = '${var.name}' TIMESERIES AUTO"
+      row           = 1
+      column        = 3
+    }
 
-  widget {
-    title         = "Cluster Memory"
-    visualization = "faceted_line_chart"
-    nrql          = "SELECT average(allocatableMemoryUtilization) AS 'Memory Utilization' FROM K8sNodeSample WHERE clusterName = '${var.name}' TIMESERIES AUTO"
-    row           = 1
-    column        = 3
-  }
+    widget_line {
+      title         = "Container CPU"
+      nrql          = "SELECT average(cpuCoresUtilization) AS 'CPU Utilization' FROM K8sContainerSample WHERE clusterName = '${var.name}' FACET deploymentName TIMESERIES AUTO"
+      row           = 2
+      column        = 1
+    }
 
-  widget {
-    title         = "Container CPU"
-    visualization = "faceted_line_chart"
-    nrql          = "SELECT average(cpuCoresUtilization) AS 'CPU Utilization' FROM K8sContainerSample WHERE clusterName = '${var.name}' FACET deploymentName TIMESERIES AUTO"
-    row           = 2
-    column        = 1
-  }
+    widget_line {
+      title         = "Container FS"
+      nrql          = "SELECT average(fsUsedPercent) AS 'FS Utilization %' FROM K8sContainerSample WHERE clusterName = '${var.name}' FACET deploymentName TIMESERIES AUTO"
+      row           = 2
+      column        = 2
+    }
 
-  widget {
-    title         = "Container FS"
-    visualization = "faceted_line_chart"
-    nrql          = "SELECT average(fsUsedPercent) AS 'FS Utilization %' FROM K8sContainerSample WHERE clusterName = '${var.name}' FACET deploymentName TIMESERIES AUTO"
-    row           = 2
-    column        = 2
-  }
-
-  widget {
-    title         = "Container Memory"
-    visualization = "faceted_line_chart"
-    nrql          = "SELECT average(memoryUtilization) AS 'Memory Utilization' FROM K8sContainerSample WHERE clusterName = '${var.name}' FACET deploymentName TIMESERIES AUTO"
-    row           = 2
-    column        = 3
+    widget_line {
+      title         = "Container Memory"
+      nrql          = "SELECT average(memoryUtilization) AS 'Memory Utilization' FROM K8sContainerSample WHERE clusterName = '${var.name}' FACET deploymentName TIMESERIES AUTO"
+      row           = 2
+      column        = 3
+    }
   }
 }
